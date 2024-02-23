4c85e47c144d654c03318e16d68cb97f7925c613
jororlando-81 <jorgeorlando81@hotmail.com>
2024-02-23 20:45:47 -0300

    created customer'endpoint to find by id

--- src/main/java/com/project/customerRegistration/controllers/CustomerController.java
+++ src/main/java/com/project/customerRegistration/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,11 @@ public class CustomerController {
 		return ResponseEntity.ok().body(listCustomers);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Customer>  findById( @PathVariable Long id){
+		Customer customer = customerService.findById(id);
+		return ResponseEntity.ok().body(customer);
+	}
 	
 }
 
--- src/main/java/com/project/customerRegistration/services/CustomerService.java
+++ src/main/java/com/project/customerRegistration/services/CustomerService.java
@@ -1,6 +1,7 @@
 package com.project.customerRegistration.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,11 @@ public class CustomerService {
 	
 	public List<Customer> findAll () {
 		return 	customerRepository.findAll();
+	}
+
+	public Customer findById ( Long id )   {
+		Optional<Customer> customer =  customerRepository.findById(id) ;
+		return customer.get();
 	}
 
 
