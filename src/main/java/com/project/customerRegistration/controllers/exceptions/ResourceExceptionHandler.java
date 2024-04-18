cf26282b64f1e03584699d8de1434e08f55de47e
jororlando-81 <jorgeorlando81@hotmail.com>
2024-04-18 18:27:21 -0300

    Exception handling for delete

--- src/main/java/com/project/customerRegistration/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/project/customerRegistration/controllers/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import java.time.Instant;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.project.customerRegistration.services.exceptions.DatabaseException;
 import com.project.customerRegistration.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -26,4 +27,16 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 		
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database( DatabaseException e ,HttpServletRequest request  ) {
+		
+		String error = "Database error" ;
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError( Instant.now() , status.value() , error , e.getMessage() ,request.getRequestURI() ) ;
+		
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
 }
